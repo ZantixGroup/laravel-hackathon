beab6a77fcfb068c952b1d1ded1f9e78b4ec4b9f
Fleksis <markuss0303@gmail.com>
2023-09-30 12:39:16 +0300

    feat: password reset

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -52,9 +53,6 @@ class UserController extends Controller
         $user->delete();
         return new UserResource($user);
     }
-//
-//    public function passwordReset(Request $request, User $user)
-//    {
-//
-//    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/password_reset/{mail}', [AuthController::class, 'passwordReset']);
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'user']);
