27db9e9f2d1482d3afc7e37907da56ccebdac178
Fleksis <markuss0303@gmail.com>
2023-09-30 18:48:07 +0300

    fix

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
         }
 
         return response()->json([
-            'error' => 'Unauthenticated'
+            'error' => 'Unauthenticated',
         ], 401);
     }
 
